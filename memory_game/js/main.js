f315cdc752455d1bc57d7af47bb33365e3fd1672
Rory Johnson <redkjohnson@gmai.com>
2017-08-09 18:58:51 -0500

    started on the actual game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[1] === cardsInPlay[0]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
