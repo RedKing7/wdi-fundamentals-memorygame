a52825f1eb9c96ba5a0d2dc97e1e9c79c5d688ce
Rory Johnson <redkjohnson@gmai.com>
2017-08-17 12:41:30 -0500

    updated javascript

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,7 +20,6 @@ var cards = [
 		cardImage: "images/king-of-hearts.png"
 	}
 ];
-
 var cardsInPlay = [];
 
 var checkForMatch = function(card1, card2){
@@ -31,15 +30,26 @@ var checkForMatch = function(card1, card2){
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suite);
+	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if(cardsInPlay.length === 2){
 		checkForMatch(cardsInPlay[0],cardsInPlay[1]);
 	}
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
