1b4bed484cbee043e984912ba98cd08bde316e61
Rory Johnson <redkjohnson@gmai.com>
2017-08-24 11:34:52 -0500

    game now works

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,23 +22,50 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+var flippedCards = [];
+
+var score = 0;
+
 var checkForMatch = function(card1, card2){
-	if(card1 === card2){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+	setTimeout(function(){
+		if(card1.rank === card2.rank){
+			alert("You found a match!");
+			score += 2;
+			if(score >= cards.length){
+				alert("You Win!");
+			}
+		} else {
+			alert("Sorry, try again.");
+			var flipBack = document.getElementsByTagName('img');
+			for(var i = 0; i<cards.length; i++){
+				var pic = flipBack[i].getAttribute('src');
+				if((pic === card1.cardImage)||(pic === card2.cardImage)){
+					flipBack[i].setAttribute('src', "images/back.png")
+				}
+			}
+		}
 	}
+	, 500);
+	cardsInPlay = [];
 }
 
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId].cardImage);
-	if(cardsInPlay.length === 2){
-		checkForMatch(cardsInPlay[0],cardsInPlay[1]);
+	if(this.getAttribute('src') === "images/back.png"){
+		console.log("User flipped " + cards[cardId].rank);
+		console.log(cards[cardId].cardImage);
+		console.log(cards[cardId].suit);
+		if(cardsInPlay.length == 1){
+			if(cards[cardId] === cardsInPlay[0]){
+				return;
+			}
+		}
+		cardsInPlay.push(cards[cardId]);
+		//cardsInPlay.push(cards[cardId].rank);
+		this.setAttribute('src', cards[cardId].cardImage);
+		if(cardsInPlay.length === 2){
+			checkForMatch(cardsInPlay[0],cardsInPlay[1]);
+		}
 	}
 }
 
@@ -51,5 +78,16 @@ var createBoard = function(){
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
+
+var reset = function(){
+	score = 0;
+	cardsInPlay = [];
+	var imgs = document.getElementsByTagName('img');
+	for(var i=0; i<imgs.length; i++){
+		imgs[i].setAttribute('src', "images/back.png")
+	}
+}
+
+document.getElementById('reset').addEventListener('click', reset);
 
 createBoard();
